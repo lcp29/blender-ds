2ac58c5beb0392f646d43fc14de2020a1bdfb1c7
helmholtz <helmholtz@fomal.host>
2023-09-16 21:00:54 +0800

    finish

--- render.py
+++ render.py
@@ -7,16 +7,68 @@ from absl import app, flags
 import json
 import math
 from tqdm import tqdm
-import time
 
 flags.DEFINE_string('scene_path', '', 'path to the Blender scene')
 flags.DEFINE_string('cam_dir', '', 'directory containing camera JSON files')
 flags.DEFINE_string('output_dir', '', 'root directory of render results')
 flags.DEFINE_integer('resx', 800, 'width of output images')
 flags.DEFINE_integer('resy', 800, 'height of output images')
+flags.DEFINE_bool('film_transparent', False, 'if the background should be transparent, '
+                                             'only influences direct background pixels')
+# output control
+flags.DEFINE_string('rgb_format', 'png', 'output format for rgb image(exr, png, nil)')
+flags.DEFINE_bool('rgba', False, 'enable alpha channel for rgb output')
+flags.DEFINE_string('alpha_format', 'nil', 'output format for alpha map(exr, png, nil)')
+flags.DEFINE_string('depth_format', 'nil', 'output format for depth map(exr, png, nil)')
+flags.DEFINE_float('depth_min', 2.0, 'minimum depth used in png format depth map')
+flags.DEFINE_float('depth_max', 6.0, 'maximum depth used in png format depth map')
+flags.DEFINE_string('normal_format', 'nil', 'output format for normal map(exr, png, nil)')
+
 
 FLAGS = flags.FLAGS
 
+def set_node_output_format(node, flag):
+    assert flag in ['exr', 'png']
+    if flag == 'exr':
+        node.format.file_format = 'OPEN_EXR'
+        node.format.color_depth = '32'
+    elif flag == 'png':
+        node.format.file_format = 'PNG'
+        node.format.color_depth = '16'
+
+def render_splits(splits, cam_intrinstics, cam_extrinstics, file_dirs, outnodes):
+    for split in splits:
+        logging.info(f'Rendering split "{split}"...')
+        # find camera in the scene
+        camera = None
+        for obj in bpy.data.objects:
+            if obj.type == 'CAMERA':
+                camera = obj
+                break
+        # if no camera in the scene, create one
+        if camera is None:
+            bpy.ops.object.camera_add()
+            camera = bpy.context.active_object
+        # set camera intrinstics
+        for k, v in cam_intrinstics[split].items():
+            setattr(camera.data, k, v)
+        logging.info(f'Camera intrinstics set: {cam_intrinstics[split]}')
+
+        # render samples
+        pbar = tqdm(range(len(file_dirs[split])))
+
+        for idx in pbar:
+            pbar.set_description(file_dirs[split][idx])
+            # set camera extrinstic
+            # blender requires transformed c2w if using numpy matrix as input
+            camera.matrix_world = cam_extrinstics[split][idx].T
+            # set output file slot
+            for outnode in outnodes:
+                outnode['node'].base_path = os.path.join(FLAGS.output_dir)
+                outnode['node'].file_slots[0].path = os.path.join(file_dirs[split][idx], f'{outnode["name"]}')
+            bpy.ops.render.render(write_still=True)
+            break
+        break
 
 def main(_):
     # delete existing output directory
@@ -91,39 +143,153 @@ def main(_):
     bpy.ops.wm.open_mainfile(filepath=FLAGS.scene_path)
 
     # set up Cycles as rendering engine
-    bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+    if FLAGS.film_transparent:
+        bpy.context.scene.render.film_transparent = True
+    else:
+        bpy.context.scene.render.film_transparent = False
+
     # use GPU
     bpy.context.scene.cycles.device = 'GPU'
     logging.info('Cycles set up as rendering engine')
 
+    # raw output
+    bpy.context.scene.view_settings.view_transform = 'Raw'
+
+    # set output composition
+    bpy.context.scene.use_nodes = True
+    node_tree = bpy.context.scene.node_tree
+    # get scene render layer
+    render_layers = None
+    for node in node_tree.nodes:
+        if node.type == 'R_LAYERS':
+            render_layers = node
+            break
+    if render_layers is None:
+        render_layers = node_tree.nodes.new("CompositorNodeRLayers")
+    # get scene view layer
+    view_layer = bpy.context.scene.view_layers[0]
+    # file output nodes
+    outnodes = []
+    # set composition
+    # RGB(A)
+    if FLAGS.rgb_format != 'nil':
+        outnode = node_tree.nodes.new("CompositorNodeOutputFile")
+        outnode.label = 'RGB Output'
+        outnode.name = 'RGB Output'
+
+        set_node_output_format(outnode, FLAGS.rgb_format)
+        outnode.format.color_mode = 'RGBA' if FLAGS.rgba else 'RGB'
+
+        render_layers.outputs['Image'].enabled = True
+        node_tree.links.new(render_layers.outputs['Image'], outnode.inputs[0])
+        outnodes.append({'node': outnode, 'name': 'color', 'ext': FLAGS.rgb_format})
+        logging.info(f'RGB{"A" if FLAGS.rgba else ""} {FLAGS.rgb_format} output enabled.')
+
     # render splits
-    for split in splits:
-        logging.info(f'Rendering split "{split}"...')
-        # find camera in the scene
-        camera = None
-        for obj in bpy.data.objects:
-            if obj.type == 'CAMERA':
-                camera = obj
-                break
-        # if no camera in the scene, create one
-        if camera is None:
-            bpy.ops.object.camera_add()
-            camera = bpy.context.active_object
-        # set camera intrinstics
-        for k, v in cam_intrinstics[split].items():
-            setattr(camera.data, k, v)
-        logging.info(f'Camera intrinstics set: {cam_intrinstics[split]}')
+    render_splits(splits, cam_intrinstics, cam_extrinstics, file_dirs, outnodes)
 
-        # render samples
-        pbar = tqdm(range(len(file_dirs[split])))
+    # clear output nodes
+    for outnode in outnodes:
+        node_tree.nodes.remove(outnode['node'])
+    outnodes = []
 
-        for idx in pbar:
-            pbar.set_description(file_dirs[split][idx])
-            # set camera extrinstic
-            # blender requires transformed c2w if use np matrix as input
-            camera.matrix_world = cam_extrinstics[split][idx].T
-            
+    # requires mask for other maps
+    bpy.context.scene.render.film_transparent = True
 
+    # foreground mask
+    # alpha
+    if FLAGS.alpha_format != 'nil':
+        outnode = node_tree.nodes.new("CompositorNodeOutputFile")
+        outnode.label = 'Alpha Output'
+        outnode.name = 'Alpha Output'
+
+        set_node_output_format(outnode, FLAGS.alpha_format)
+        #outnode.format.color_mode = 'BW' if FLAGS.alpha_format == 'png' else 'RGBA'
+
+        render_layers.outputs['Alpha'].enabled = True
+        rgb_compositor = node_tree.nodes.new("CompositorNodeCombineColor")
+        node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[0])
+        node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[1])
+        node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[2])
+        node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[3])
+        node_tree.links.new(rgb_compositor.outputs[0], outnode.inputs[0])
+        outnodes.append({'node': outnode, 'name': 'alpha', 'ext': FLAGS.alpha_format})
+        logging.info(f'Alpha {FLAGS.alpha_format} output enabled.')
+    
+    # depth
+    if FLAGS.depth_format != 'nil':
+        outnode = node_tree.nodes.new("CompositorNodeOutputFile")
+        outnode.label = 'Depth Output'
+        outnode.name = 'Depth Output'
+
+        set_node_output_format(outnode, FLAGS.depth_format)
+
+        view_layer.use_pass_z = True
+        render_layers.outputs['Depth'].enabled = True
+        if FLAGS.depth_format == 'exr':
+            # directly connect depth output with file node input with alpha channel
+            rgb_compositor = node_tree.nodes.new("CompositorNodeCombineColor")
+            node_tree.links.new(render_layers.outputs['Depth'], rgb_compositor.inputs[0])
+            node_tree.links.new(render_layers.outputs['Depth'], rgb_compositor.inputs[1])
+            node_tree.links.new(render_layers.outputs['Depth'], rgb_compositor.inputs[2])
+            node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[3])
+            node_tree.links.new(rgb_compositor.outputs[0], outnode.inputs[0])
+        elif FLAGS.depth_format == 'png':
+            # requires some compression
+            depth_map = node_tree.nodes.new(type="CompositorNodeMapRange")
+            depth_map.inputs['From Min'].default_value = FLAGS.depth_min
+            depth_map.inputs['From Max'].default_value = FLAGS.depth_max
+            depth_map.inputs['To Min'].default_value = 0
+            depth_map.inputs['To Max'].default_value = 1
+            node_tree.links.new(render_layers.outputs['Depth'], depth_map.inputs[0])
+            rgb_compositor = node_tree.nodes.new("CompositorNodeCombineColor")
+            node_tree.links.new(depth_map.outputs[0], rgb_compositor.inputs[0])
+            node_tree.links.new(depth_map.outputs[0], rgb_compositor.inputs[1])
+            node_tree.links.new(depth_map.outputs[0], rgb_compositor.inputs[2])
+            node_tree.links.new(render_layers.outputs['Alpha'], rgb_compositor.inputs[3])
+            node_tree.links.new(rgb_compositor.outputs[0], outnode.inputs[0])
+
+        outnodes.append({'node': outnode, 'name': 'depth', 'ext': FLAGS.depth_format})
+        logging.info(f'Depth {FLAGS.depth_format} output enabled.')
+    
+    # normal
+    if FLAGS.normal_format != 'nil':
+        outnode = node_tree.nodes.new("CompositorNodeOutputFile")
+        outnode.label = 'Normal Output'
+        outnode.name = 'Normal Output'
+
+        set_node_output_format(outnode, FLAGS.normal_format)
+        outnode.format.color_mode = 'RGBA'
+
+        view_layer.use_pass_normal = True
+        render_layers.outputs['Normal'].enabled = True
+        if FLAGS.normal_format == 'exr':
+            node_tree.links.new(render_layers.outputs['Normal'], outnode.inputs[0])
+        else:
+            # transform from [-1, 1] to [0, 1] on all channels for png
+            rgb_separator = node_tree.nodes.new("CompositorNodeSeparateColor")
+            muladds = [node_tree.nodes.new("CompositorNodeMath") for _ in range(3)]
+            rgb_combiner = node_tree.nodes.new("CompositorNodeCombineColor")
+            # connect normal output to rgb_seperator
+            node_tree.links.new(render_layers.outputs['Normal'], rgb_separator.inputs[0])
+            # connect muladd units
+            for idx, m in enumerate(muladds):
+                m.operation = 'MULTIPLY_ADD'    # operation
+                m.inputs[1].default_value = 0.5 # scale factor
+                m.inputs[2].default_value = 0.5 # bias
+                node_tree.links.new(rgb_separator.outputs[idx], m.inputs[0])
+                node_tree.links.new(m.outputs[0], rgb_combiner.inputs[idx])
+            # add alpha channel to normal map
+            node_tree.links.new(render_layers.outputs['Alpha'],rgb_combiner.inputs[3])
+            # connect to the final output
+            node_tree.links.new(rgb_combiner.outputs[0], outnode.inputs[0])
+
+        outnodes.append({'node': outnode, 'name': 'normal', 'ext': FLAGS.normal_format})
+        logging.info(f'Normal {FLAGS.alpha_format} output enabled.')
+
+    # render splits
+    render_splits(splits, cam_intrinstics, cam_extrinstics, file_dirs, outnodes)
 
 if __name__ == '__main__':
     argv = sys.argv
